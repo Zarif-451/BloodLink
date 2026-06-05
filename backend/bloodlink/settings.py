ef7bba243e1443a64aa57277ca27cc53c856576a
Muhammad Zarif Rahman <zarifrahman451@gmail.com>
2026-06-05 18:11:55 +0600

    Configure PostgreSQL Database

--- backend/bloodlink/settings.py
+++ backend/bloodlink/settings.py
@@ -73,9 +73,13 @@ WSGI_APPLICATION = 'bloodlink.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "bloodlink",
+        "USER": "postgres",
+        "PASSWORD": "YOUR_PASSWORD_HERE",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
