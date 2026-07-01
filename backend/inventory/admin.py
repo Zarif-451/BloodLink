2ac6eb64a8b13b82e4cd6a6c80d7c0af2392eefe
Muhammad Zarif Rahman <zarifrahman451@gmail.com>
2026-07-01 09:57:36 +0600

    Configure Django admin and complete initial migrations

--- backend/inventory/admin.py
+++ backend/inventory/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import BloodInventory, Allocation
 
-# Register your models here.
+admin.site.register(BloodInventory)
+admin.site.register(Allocation)
--- backend/requests/admin.py
+++ backend/requests/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Requester, Request
 
-# Register your models here.
+admin.site.register(Requester)
+admin.site.register(Request)
