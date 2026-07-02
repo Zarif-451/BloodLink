cf7bbb1470f070b23450db705bc53130192d3a66
Muhammad Zarif Rahman <zarifrahman451@gmail.com>
2026-07-02 10:12:28 +0600

    Finished Donor CRUD

--- backend/donors/urls.py
+++ backend/donors/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import DonorListAPIView
+from .views import DonorListAPIView, DonorDetailAPIView
 
 urlpatterns = [
-    path("", DonorListAPIView.as_view(), name="donor-list")
+    path("", DonorListAPIView.as_view(), name="donor-list"),
+
+    path(
+    "<str:national_ID>/",
+    DonorDetailAPIView.as_view(),
+    name="donor-detail"
+)
 ]
--- backend/donors/views.py
+++ backend/donors/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from .models import Donor
 from .serializers import DonorSerializer
+
+from rest_framework import status
+
 
 class DonorListAPIView(APIView):
 
@@ -14,4 +17,103 @@ class DonorListAPIView(APIView):
         serializer = DonorSerializer(donors, many=True)
 
         return Response(serializer.data)
+    
 
+    def post(self, request):
+
+        serializer = DonorSerializer(data=request.data)
+
+        if serializer.is_valid():
+
+            serializer.save()
+
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+        
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class DonorDetailAPIView(APIView):
+
+    def get(self, request, national_ID):
+
+        donor = get_object_or_404(
+            Donor,
+            national_ID=national_ID
+        )
+        
+        serializer = DonorSerializer(donor)
+        
+        return Response(serializer.data)
+
+    
+    def put(self, request, national_ID):
+
+        donor = get_object_or_404(
+            Donor,
+            national_ID=national_ID
+        )
+
+        serializer = DonorSerializer(
+            instance=donor,
+            data=request.data
+        )
+
+        if serializer.is_valid():
+            
+            serializer.save()
+
+            return Response(
+                serializer.data,
+                status=status.HTTP_200_OK
+            )
+        
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    
+    def patch(self, request, national_ID):
+
+        donor = get_object_or_404(
+            Donor,
+            national_ID=national_ID
+        )
+
+        serializer = DonorSerializer(
+            instance=donor,
+            data=request.data,
+            partial=True
+        )
+
+        if serializer.is_valid():
+            
+            serializer.save()
+
+            return Response(
+                serializer.data,
+                status=status.HTTP_200_OK
+            )
+        
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    
+    def delete(self, request, national_ID):
+
+        donor = get_object_or_404(
+            Donor,
+            national_ID=national_ID
+        )
+
+        donor.delete()
+
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )
